017838bedb964c0c15ff048d10ae87ed32fbfe29
Geethabalan99 <geethabalanoff@gmail.com>
2025-08-16 22:25:21 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name="John Doe";
+		int age=30;
+		float salary=55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobilenumber=9876543210L;
+		int pinCode=56001;	
+		byte  luckyNumber=7;
+		
+		System.out.println(name+"\n"+age+"\n"+salary+"\n"
+				+gender+"\n"+acceptTerms+"\n"+rating
+				+"\n"+mobilenumber+"\n"+pinCode+"\n"+luckyNumber);
+		
+		
+		
+		
+
+	}
+
+}
