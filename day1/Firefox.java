443fc2b6ec71056ed4d2fbfe3ceb75fd0d886c05
Geethabalan99 <geethabalanoff@gmail.com>
2025-08-16 22:56:29 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		float version=100.2f;
+		String  name="firefox";
+		boolean visibility=true;
+		int year=1998;
+		char logo='f';
+		
+		System.out.println("Browser Version = "+100.2 +"\n"+ "Browser Name = " +name +"\n"+
+		"Is visible = "+visibility +"\n"+ "Release Year = " +year +"\n"+ "Browser Logo = "+logo);
+		
+		
+		
+		
+
+	}
+
+}
